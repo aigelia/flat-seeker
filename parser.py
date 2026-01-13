4782ddf41798c11e4eec28348d31a3c3d4336f26
Katerina <e.mayakovskaya@gmail.com>
2026-01-13 22:39:49 +0200

    Fix

--- parser.py
+++ parser.py
@@ -67,6 +67,15 @@ class AruodasParser:
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
+
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-software-rasterizer")
+        options.add_argument("--disable-dev-tools")
+        options.add_argument("--no-first-run")
+        options.add_argument("--no-zygote")
+        options.add_argument("--single-process")
+        options.add_argument("--remote-debugging-port=9222")
+
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.add_experimental_option("useAutomationExtension", False)
 
