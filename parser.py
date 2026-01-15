2cce13b7a7e8e5612dd995c6ac34cd4ab45f0141
Katerina <e.mayakovskaya@gmail.com>
2026-01-15 11:12:44 +0200

    Add binary location of driver

--- parser.py
+++ parser.py
@@ -61,6 +61,8 @@ class AruodasParser:
         if headless:
             options.add_argument("--headless=new")
 
+        options.binary_location = "/snap/bin/chromium"
+
         # Обход детекции
         options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_argument("--no-sandbox")
